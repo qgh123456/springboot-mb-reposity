7ff62a826a49193a6b1f1fd7a3a1e91df7c8d9f3
qiguohui1 <qigh@belink.com>
2019-07-23 11:33:55 +0800

    springboot+mybatis的完整代码提交

--- src/main/java/com/atqgh/springboot/SpringbootApplication.java
+++ src/main/java/com/atqgh/springboot/SpringbootApplication.java
@@ -1,8 +1,12 @@
 package com.atqgh.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@MapperScan(value = "com.atqgh.springboot.mapper")
+@ServletComponentScan(value = "com.atqgh.springboot.config")
 @SpringBootApplication
 public class SpringbootApplication {
 
---
+++ src/main/java/com/atqgh/springboot/bean/Demo2.java
@@ -0,0 +1,28 @@
+package com.atqgh.springboot.bean;
+
+/**
+ * @Author: qiguohui
+ * @Description:
+ * @Date: Created in 9:56 2019/5/31
+ * @Modified By:
+ **/
+public class Demo2 {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
